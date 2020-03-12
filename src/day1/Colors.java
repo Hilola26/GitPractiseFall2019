1589c11f0703305e241f46ccb7027116fd2f01ac
Hilola26 <alrahiym78@gmail.com>
2020-03-11 20:29:48 -0400

    update

--- src/day1/Colors.java
+++ src/day1/Colors.java
@@ -1,4 +1,7 @@
 package day1;
 
 public class Colors {
-}
+    public static void main(String[] args) {
+      String colors[]={"red","blue","green"};
+        System.out.println("My colors "+colors);
+    }}
